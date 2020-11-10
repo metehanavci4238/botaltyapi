e7658f583b25e45f4a1e6646b12136d50d7559bb
Glitch (dc-patlatma-botumuz) <none>
2020-11-10 09:55:04 +0000

    🎉⛸ Checkpoint ./events/ready.js:20492095/1469

--- events/ready.js
+++ events/ready.js
@@ -6,23 +6,7 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-var oyun = [
-        "Metehan ❤",
-        "Metehan ❤",
-        "Metehan ❤"  
-    ];
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "Metehan ❤" );
-        }, 2 * 2500);
-    
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  client.user.setActivity(`Metehan ❤`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+client.user.setActivity("Metehan ❤️")
+client.user.setStatus("dnd")
+console.log("Bot aktif, s ve j gibi kadim olun.")
 };
